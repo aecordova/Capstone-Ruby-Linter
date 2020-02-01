37b8471d5bcac65e6050f865d46a92f7ac58194a
acordova <ae_cordova@outlook.com>
2020-02-01 15:14:46 -0600

    Added spacing cop completed

--- bin/main.rb
+++ bin/main.rb
@@ -12,22 +12,6 @@ kw = ['{']
 b = Buffer.new(file_path)
 parsed_file = Parser.new(b.content,kw)
 
-def spacing_cop(parsed_file)
-  l = 0
-  parsed_file.blocks.each do |x|
-    line, s = x[0], x[2]
-    spc_check_before(line, s, '\)')
-    # spc_check_after(line, s, '\)')
-    # spc_check_after(line, s, ',')
-    # spc_check_after(line, s, ':')
-  end
-end
 
-def spc_check_before(line, str, char)
-  str.scan_until(Regexp.new(char))
-  while str.matched?
-    str.scan(/\s+/)
-    log_error(2, line, char, str.pos) if str.matched != ' '
-    str.scan_until(Regexp.new(char))
-  end
-end
+spacing_cop(parsed_file)
+indent_cop(parsed_file)
--- lib/cops.rb
+++ lib/cops.rb
@@ -2,6 +2,7 @@
 module Cops
   def indent_cop(parsed_file)
     parsed_file.blocks.each do |x|
+      x[2].reset
       sp = x[2].scan(/\s+/)
       if sp.nil?
         sp = 0
@@ -16,13 +17,28 @@ module Cops
     l = 0
     parsed_file.blocks.each do |x|
       line, s = x[0], x[2]
+      spc_check_before(line, s, '{')
+      spc_check_before(line, s, '\(')
       spc_check_after(line, s, '\)')
       spc_check_after(line, s, ',')
       spc_check_after(line, s, ':')
     end
   end
   
+  def spc_check_before(line, str, char)
+    str.reset
+    s = str.scan_until(Regexp.new(char))
+    while str.matched?
+      s = StringScanner.new(s.reverse)
+      s.skip(Regexp.new(char))
+      s.scan(/\s+/)
+      log_error(3, line, char,s.string.length - s.pos) if s.matched != ' '
+      s = str.scan_until(Regexp.new(char))
+    end
+  end
+
   def spc_check_after(line, str, char)
+    str.reset
     str.scan_until(Regexp.new(char))
     while str.matched?
       str.scan(/\s+/)
@@ -31,15 +47,16 @@ module Cops
     end
   end
 
-
-  def log_error(type, line, char = nil,pos=nil)
+  def log_error(type, line, char = nil, pos = nil)
     err_string = "Error: line #{line}"
-    err_string +=", col: #{pos} " unless pos.nil?
+    err_string += ", col: #{pos}" unless pos.nil?
     case type
     when 1
       puts "#{err_string}, Wrong Indentation "
     when 2
       puts "#{err_string}, Wrong spacing after '#{char}' "
+    when 3
+      puts "#{err_string}, Wrong spacing before '#{char}' "
     else
       puts "Other"
     end
