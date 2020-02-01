bf1226dfe04949ba4c8618aa926cf05716e59fe3
Angel Cordova <ae_cordova@outlook.com>
2020-02-01 10:29:29 -0600

    Added cop to check single space after colons

--- bin/main.rb
+++ bin/main.rb
@@ -7,18 +7,12 @@ require 'strscan'
 
 include Cops
 
-file_path = '/home/ae_co/Documents/Microverse/Ruby_Course/Capstone-Ruby-Linter/example'
+file_path = '../example'
 kw = ['{']
 b = Buffer.new(file_path)
 parsed_file = Parser.new(b.content,kw)
 
-indent_cop(parsed_file)
+# indent_cop(parsed_file)
 
-# def spacing_cop(parsed_file)
-#   parsed_file.blocks.each do |x|
-#     x[2].scan_until(/:/)
-#     p x[2].matched
-#   end
-# end
-
-# spacing_cop(parsed_file)
+ 
+ spacing_cop(parsed_file)
--- lib/cops.rb
+++ lib/cops.rb
@@ -12,11 +12,28 @@ module Cops
     end
   end
 
+  def spacing_cop(parsed_file)
+    l = 0
+    parsed_file.blocks.each do |x|
+      line, s = x[0], x[2]
+      s.scan_until(/:/)
+      next unless s.matched?
 
-  def log_error(type, line)
+      s.scan(/\s+/)
+      l = 0 if s.matched.nil?
+      l = s.matched.length unless s.matched.nil?
+      log_error(2,line,':') if l != 1
+    end
+  end
+
+
+  def log_error(type, line, char = nil)
+    err_string = "Error:line #{line}"
     case type
     when 1
-      puts "Error: Wrong Indentation  on line #{line}"
+      puts "#{err_string}, Wrong Indentation "
+    when 2
+      puts "#{err_string}, Wrong spacing after '#{char}' "
     else
       puts "Other"
     end
