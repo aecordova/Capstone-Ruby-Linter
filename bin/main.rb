fe68c1bb15664d9810099c59c742b7e61368ea87
acordova <ae_cordova@outlook.com>
2020-01-31 19:00:16 -0600

    Added Indentation cop

--- bin/main.rb
+++ bin/main.rb
@@ -4,3 +4,11 @@
 require_relative '../lib/parser.rb'
 require 'strscan'
 
+file_path = '/home/ae_co/Documents/Microverse/Ruby_Course/Capstone-Ruby-Linter/example'
+kw = ['{']
+b = Buffer.new(file_path)
+parsed_file = Parser.new(b.content,kw)
+
+
+
+indent_cop(parsed_file)
