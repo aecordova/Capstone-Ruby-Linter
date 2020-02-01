12a6f2bc9e330dc291b1317cecf41195cbc595e0
acordova <ae_cordova@outlook.com>
2020-01-31 19:01:24 -0600

    Added Indentation cop

---
+++ lib/cops.rb
@@ -0,0 +1,23 @@
+module Cops
+    def indent_cop(parsed_file)
+        parsed_file.blocks.each do |x|
+          sp = x[2].scan(/\s+/)
+          if sp.nil?
+            sp = 0
+          else
+            sp = sp.length
+          end 
+          log_error(1,x[0]) unless sp ==  x[1]*2
+        end
+      end
+    end
+      
+    def log_error(type, line)
+      case type
+      when 1
+        puts "Error: Wrong Indentation  on line #{line}"
+      else
+        return
+      end
+    end
+end
