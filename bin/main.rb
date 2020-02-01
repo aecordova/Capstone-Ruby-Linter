74923b49b7caf35cf1d7682587cb8ea78bcc663a
acordova <ae_cordova@outlook.com>
2020-01-31 22:49:26 -0600

    Adding cop for spacing still in progress

--- bin/main.rb
+++ bin/main.rb
@@ -2,13 +2,23 @@
 # frozen_string_literal: true
 
 require_relative '../lib/parser.rb'
+require_relative '../lib/cops.rb'
 require 'strscan'
+
+include Cops
 
 file_path = '/home/ae_co/Documents/Microverse/Ruby_Course/Capstone-Ruby-Linter/example'
 kw = ['{']
 b = Buffer.new(file_path)
 parsed_file = Parser.new(b.content,kw)
 
-
-
 indent_cop(parsed_file)
+
+# def spacing_cop(parsed_file)
+#   parsed_file.blocks.each do |x|
+#     x[2].scan_until(/:/)
+#     p x[2].matched
+#   end
+# end
+
+# spacing_cop(parsed_file)
--- lib/cops.rb
+++ lib/cops.rb
@@ -1,23 +1,24 @@
+
 module Cops
-    def indent_cop(parsed_file)
-        parsed_file.blocks.each do |x|
-          sp = x[2].scan(/\s+/)
-          if sp.nil?
-            sp = 0
-          else
-            sp = sp.length
-          end 
-          log_error(1,x[0]) unless sp ==  x[1]*2
-        end
-      end
-    end
-      
-    def log_error(type, line)
-      case type
-      when 1
-        puts "Error: Wrong Indentation  on line #{line}"
+  def indent_cop(parsed_file)
+    parsed_file.blocks.each do |x|
+      sp = x[2].scan(/\s+/)
+      if sp.nil?
+        sp = 0
       else
-        return
-      end
+        sp = sp.length
+      end 
+      log_error(1,x[0]) unless sp ==  x[1]*2
     end
+  end
+
+
+  def log_error(type, line)
+    case type
+    when 1
+      puts "Error: Wrong Indentation  on line #{line}"
+    else
+      puts "Other"
+    end
+  end
 end
