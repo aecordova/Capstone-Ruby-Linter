f47c10a017598d09d67669ee0b827f0107f9eec0
acordova <ae_cordova@outlook.com>
2020-02-02 23:32:13 -0600

    Added chck for sigle empty line after after }

--- bin/main.rb
+++ bin/main.rb
@@ -10,23 +10,12 @@ include Cops
 file_path = '../example'
 kw = ['{']
 b = Buffer.new(file_path)
-parsed_file = Parser.new(b.content,kw)
+parsed_file = Parser.new(b.content_s, kw)
 
 
-# line_format_cop(parsed_file)
-# spacing_cop(parsed_file)
-# indent_cop(parsed_file)
+line_format_cop(parsed_file)
+spacing_cop(parsed_file)
+indent_cop(parsed_file)
 
-def check_lines_bet_blocks(blocks)
-  blocks.each_with_index do |b,i|
-    next unless b[2].exist?(/}/)
-    n = i
-    spct = 0
-     if b[2].matched?
-      p blocks[n+1][2].string==""
 
-     end
-  end
-end
-
-check_lines_bet_blocks(parsed_file.blocks)
+# check_lines_bet_blocks(parsed_file.blocks,'}')
