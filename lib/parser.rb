0e99e91e8db67a5727c8888706acc8289e4a0634
acordova <ae_cordova@outlook.com>
2020-02-03 09:10:40 -0600

    Corrected linter errors

--- lib/parser.rb
+++ lib/parser.rb
@@ -55,12 +55,14 @@ class Buffer
 
   private
 
+  # rubocop: disable Lint/UselessAssignment
   def get_file_content(file_path)
     content_s = ''
     File.open(file_path, 'r') { |f| content_s = f.readlines.map(&:chomp) }
     content_scan = content_s.map { |v| v = StringScanner.new(v) }
     content_scan
   end
+  # rubocop: enable Lint/UselessAssignment
 end
 # This class returns an array of string scanners,
 # with the identified level of indentation, line number and String scanners
